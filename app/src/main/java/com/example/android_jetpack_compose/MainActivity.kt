f0d6862b6b6d9ed784351dc4e03edb44d00d1b85
OhDani <tranngoc2k41511gmail.com>
2024-03-06 22:28:24 +0700

    taskCompleted

--- app/src/main/java/com/example/android_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/example/android_jetpack_compose/MainActivity.kt
@@ -5,9 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -15,72 +16,58 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.android_jetpack_compose.ui.theme.ComposeArticleTheme
+import com.example.android_jetpack_compose.ui.theme.TaskCompletedTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeArticleTheme{
-                Surface (
+            TaskCompletedTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ){
-                    ComposeArticleApp()
+                ) {
+                    TaskCompletedScreen()
                 }
             }
         }
     }
 }
-@Composable
-fun ComposeArticleApp() {
-    ArticleCard(
-        title = stringResource(R.string.title_jetpack_compose_tutorial),
-        shortDescription = stringResource(R.string.compose_short_desc),
-        longDescription = stringResource(R.string.compose_long_desc),
-        imagePainter = painterResource(R.drawable.bg_compose_background)
-    )
-}
 
 @Composable
-private fun ArticleCard(
-    title: String,
-    shortDescription: String,
-    longDescription: String,
-    imagePainter: Painter,
-    modifier: Modifier = Modifier,
-) {
-    Column(modifier = modifier) {
-        Image(painter = imagePainter, contentDescription = null)
+fun TaskCompletedScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        val image = painterResource(R.drawable.ic_task_completed)
+        Image(painter = image, contentDescription = null)
         Text(
-            text = title,
-            modifier = Modifier.padding(16.dp),
-            fontSize = 24.sp
+            text = stringResource(R.string.all_task_completed),
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp),
+            fontWeight = FontWeight.Bold
         )
         Text(
-            text = shortDescription,
-            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
-            textAlign = TextAlign.Justify
-        )
-        Text(
-            text = longDescription,
-            modifier = Modifier.padding(16.dp),
-            textAlign = TextAlign.Justify
+            text = stringResource(R.string.nice_work),
+            fontSize = 16.sp
         )
     }
 }
+
 @Preview(showBackground = true)
 @Composable
-fun ComposeArticleAppPreview(){
-    ComposeArticleTheme{
-        ComposeArticleApp()
+fun TaskCompletedPreview() {
+    TaskCompletedTheme {
+        TaskCompletedScreen()
     }
 }
