3bca45275a225065b002b001d5db988d57790a3e
OhDani <tranngoc2k41511gmail.com>
2024-03-19 22:30:51 +0700

    ArtSpace

--- app/src/main/java/com/example/android_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/example/android_jetpack_compose/MainActivity.kt
@@ -7,7 +7,13 @@ import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.annotation.VisibleForTesting
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -22,6 +28,7 @@ import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.AlertDialogDefaults.shape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -35,13 +42,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.android_jetpack_compose.ui.theme.TipTimeTheme
+import com.example.android_jetpack_compose.ui.theme.ArtSpaceTheme
 import java.text.NumberFormat
 import kotlin.math.ceil
 
@@ -50,11 +60,14 @@ class MainActivity : ComponentActivity() {
 //        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
-            TipTimeTheme {
+            ArtSpaceTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.secondary),
+                    color = MaterialTheme.colorScheme.background,
                 ) {
-                    TipTimeLayout()
+                    ArtSpaceLayout()
                 }
             }
         }
@@ -62,14 +75,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TipTimeLayout() {
-    var amountInput by remember { mutableStateOf("") }
-    var tipInput by remember { mutableStateOf("") }
-    var roundUp by remember { mutableStateOf(false) }
-
-    val amount = amountInput.toDoubleOrNull() ?: 0.0
-    val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
-    val tip = calculateTip(amount, tipPercent, roundUp)
+fun ArtSpaceLayout () {
+    var currentIndex by remember { mutableStateOf(0) }
+    val imageList = listOf(R.drawable.art1, R.drawable.art2, R.drawable.art3)
+    val authorList = listOf(R.string.art_author1, R.string.art_author2, R.string.art_author3) // Danh sách tác giả
+    val artList = listOf(R.string.art_1, R.string.art_2, R.string.art_3)
 
     Column(
         modifier = Modifier
@@ -80,102 +90,76 @@ fun TipTimeLayout() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = stringResource(R.string.calculate_tip),
+        Box(
             modifier = Modifier
-                .padding(bottom = 16.dp, top = 40.dp)
-                .align(alignment = Alignment.Start)
-        )
-        EditNumberField(
-            label = R.string.bill_amount,
-            leadingIcon = R.drawable.money,
-            keyboardOptions = KeyboardOptions.Default.copy(
-                keyboardType = KeyboardType.Number,
-                imeAction = ImeAction.Next
-            ),
-            value = amountInput,
-            onValueChanged = { amountInput = it },
-            modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth(),
-        )
-        EditNumberField(
-            label = R.string.how_was_the_service,
-            leadingIcon = R.drawable.percent,
-            keyboardOptions = KeyboardOptions.Default.copy(
-                keyboardType = KeyboardType.Number,
-                imeAction = ImeAction.Done
-            ),
-            value = tipInput,
-            onValueChanged = { tipInput = it },
-            modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth(),
-        )
-        RoundTheTipRow(
-            roundUp = roundUp,
-            onRoundUpChanged = { roundUp = it },
-            modifier = Modifier.padding(bottom = 32.dp)
-        )
+                .padding(10.dp)
+                .shadow(15.dp)
+        ) {
+            Surface(
+                modifier = Modifier
+                    .clickable {
+                        currentIndex = (currentIndex - 1).coerceAtLeast(0)
+                    }
+                    .fillMaxSize()
+            ) {
+                Image(
+                    painter = painterResource(id = imageList[currentIndex]),
+                    contentDescription = "Art Image",
+
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(40.dp))
         Text(
-            text = stringResource(R.string.tip_amount, tip),
-            style = MaterialTheme.typography.displaySmall
-        )
-        Spacer(modifier = Modifier.height(150.dp))
-    }
-}
-
-@Composable
-fun EditNumberField(
-    @StringRes label: Int,
-    @DrawableRes leadingIcon: Int,
-    keyboardOptions: KeyboardOptions,
-    value: String,
-    onValueChanged: (String) -> Unit,
-    modifier: Modifier = Modifier
-) {
-    TextField(
-        value = value,
-        singleLine = true,
-        leadingIcon = { Icon(painter = painterResource(id = leadingIcon), null) },
-        modifier = modifier,
-        onValueChange = onValueChanged,
-        label = { Text(stringResource(label)) },
-        keyboardOptions = keyboardOptions
-    )
-}
-
-@Composable
-fun RoundTheTipRow(
-    roundUp: Boolean,
-    onRoundUpChanged: (Boolean) -> Unit,
-    modifier: Modifier = Modifier
-) {
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .size(48.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(text = stringResource(R.string.round_up_tip))
-        Switch(
+            text = "${stringResource(id = artList[currentIndex])}\n${stringResource(id = authorList[currentIndex])}",
+            style = MaterialTheme.typography.bodyLarge,
             modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentWidth(Alignment.End),
-            checked = roundUp,
-            onCheckedChange = onRoundUpChanged
+                .background(color = Color.LightGray, shape = RectangleShape)
+                .clickable {
+                    currentIndex = (currentIndex - 1).coerceAtLeast(0)
+                }
+                .padding(8.dp)
         )
-    }
-}
-@VisibleForTesting
-internal fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String {
-    var tip = tipPercent / 100 * amount
-    if (roundUp) {
-        tip = ceil(tip)
-    }
-    return NumberFormat.getCurrencyInstance().format(tip)
-}
+        Spacer(modifier = Modifier.height(20.dp))
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            // Button previous
+            Surface(
+                modifier = Modifier
+                    .border(BorderStroke(2.dp, Color.Blue), shape)
+                    .padding(horizontal = 20.dp, vertical = 8.dp),
+//                    .size(100.dp, 50.dp)
+//                    .align(Alignment.CenterVertically),
+                onClick = {
+                    currentIndex = (currentIndex - 1).coerceAtLeast(0)
+                }
+            ) {
+                Text(text = "Previous", modifier = Modifier.align(Alignment.CenterVertically))
+            }
 
-@Preview(showBackground = true)
+            // Button next
+            Surface(
+                modifier = Modifier
+                    .border(BorderStroke(2.dp, Color.Blue), shape)
+                    .padding(horizontal = 20.dp, vertical = 8.dp),
+//                    .size(100.dp, 50.dp)
+//                    .align(Alignment.CenterVertically),
+                onClick = {
+                    currentIndex = (currentIndex + 1).coerceAtMost(imageList.size - 1)
+                }
+            ) {
+                Text(text = "Next", modifier = Modifier.align(Alignment.CenterVertically))
+            }
+        }
+    }
+}
+@Preview
 @Composable
-fun TipTimeLayoutPreview() {
-    TipTimeTheme {
-        TipTimeLayout()
+fun ArtSpaceLayoutTheme(){
+    ArtSpaceTheme {
+        ArtSpaceLayout()
     }
 }
