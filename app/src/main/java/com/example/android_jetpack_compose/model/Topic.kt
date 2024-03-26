e4dd73a9b67a5ada794cc2e5f5b5dd32ea3e7768
OhDani <tranngoc2k41511gmail.com>
2024-03-26 15:42:56 +0700

    GridList

--- app/src/main/java/com/example/android_jetpack_compose/model/Topic.kt
+++ app/src/main/java/com/example/android_jetpack_compose/model/Topic.kt
@@ -1,9 +1,9 @@
 package com.example.android_jetpack_compose.model
-
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
-data class Affirmation(
-    @StringRes val stringResourceId: Int,
-    @DrawableRes val imageResourceId: Int
+data class Topic(
+    @StringRes val name: Int,
+    val availableCourses: Int,
+    @DrawableRes val imageRes: Int
 )
